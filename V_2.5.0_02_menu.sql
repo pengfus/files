33562a064097474a8ab2550d979faeefb90b64d6
Bryce <pfsheng@aoyuan.net>
2020-03-26 18:41:08 +0800

    更新菜单顺序

--- V_2.5.0_02_menu.sql
+++ V_2.5.0_02_menu.sql
@@ -2,6 +2,10 @@
 
 use o2o;
 
+update o2o.sys_menu
+set icon = 'fa fa-table'
+where name = '营运管理'
+  and parent_id = 0;
 
 insert into o2o.sys_menu (menu_id, parent_id, name, url, perms, type, icon, order_num, pc_showed, app_code, app_order_num, remark, sys_belong) values (818, 775, '完成任务', null, 'operation:tasks:status:patch', 2, null, 0, 1, null, null, null, 'o2o');
 insert into o2o.sys_menu (menu_id, parent_id, name, url, perms, type, icon, order_num, pc_showed, app_code, app_order_num, remark, sys_belong) values (817, 772, '导出任务完成情况', null, 'operation:task:definitions:details:groupByTaskIdCollectTime:excel,operation:task:groups:details:groupByTaskIdCollectTime:excel', 2, null, 0, 1, null, null, null, 'o2o');
@@ -43,11 +47,11 @@ insert into o2o.sys_menu (menu_id, parent_id, name, url, perms, type, icon, orde
 insert into o2o.sys_menu (menu_id, parent_id, name, url, perms, type, icon, order_num, pc_showed, app_code, app_order_num, remark, sys_belong) values (784, 768, '查询', null, 'staff:query', 2, null, 0, 1, null, null, null, 'o2o');
 insert into o2o.sys_menu (menu_id, parent_id, name, url, perms, type, icon, order_num, pc_showed, app_code, app_order_num, remark, sys_belong) values (783, 768, '修改', null, 'staff:edit', 2, null, 0, 1, null, null, null, 'o2o');
 insert into o2o.sys_menu (menu_id, parent_id, name, url, perms, type, icon, order_num, pc_showed, app_code, app_order_num, remark, sys_belong) values (782, 768, '分配店铺职能', null, 'staff:distribution', 2, null, 0, 1, null, null, null, 'o2o');
-insert into o2o.sys_menu (menu_id, parent_id, name, url, perms, type, icon, order_num, pc_showed, app_code, app_order_num, remark, sys_belong) values (775, 554, '任务列表', 'modules/operationManage/stoTaskList.html', 'operation:tasks:get,operation:task:groups:get', 1, null, 9, 1, 'APP_MENU_OPERATION_TASK_', null, null, 'o2o');
-insert into o2o.sys_menu (menu_id, parent_id, name, url, perms, type, icon, order_num, pc_showed, app_code, app_order_num, remark, sys_belong) values (774, 554, '物料申请', 'modules/operationManage/stoMaterialRequisition.html', null, 1, null, 8, 1, null, null, null, 'o2o');
-insert into o2o.sys_menu (menu_id, parent_id, name, url, perms, type, icon, order_num, pc_showed, app_code, app_order_num, remark, sys_belong) values (772, 554, '任务管理', 'modules/operationManage/heaTaskManage.html', 'operation:task:definitions:get,operation:task:groups:get', 1, null, 10, 1, null, null, null, 'o2o');
-insert into o2o.sys_menu (menu_id, parent_id, name, url, perms, type, icon, order_num, pc_showed, app_code, app_order_num, remark, sys_belong) values (771, 554, '任务组设置', 'modules/operationManage/heaTaskGroupSettings.html', 'operation:task:groups:get', 1, null, 9, 1, null, null, null, 'o2o');
-insert into o2o.sys_menu (menu_id, parent_id, name, url, perms, type, icon, order_num, pc_showed, app_code, app_order_num, remark, sys_belong) values (770, 554, '物料申请管理', 'modules/operationManage/heaMaterialApplicationManage.html', null, 1, null, 9, 1, null, null, null, 'o2o');
+insert into o2o.sys_menu (menu_id, parent_id, name, url, perms, type, icon, order_num, pc_showed, app_code, app_order_num, remark, sys_belong) values (775, 554, '任务列表', 'modules/operationManage/stoTaskList.html', 'operation:tasks:get,operation:task:groups:get', 1, null, 33, 1, 'APP_MENU_OPERATION_TASK_', null, null, 'o2o');
+insert into o2o.sys_menu (menu_id, parent_id, name, url, perms, type, icon, order_num, pc_showed, app_code, app_order_num, remark, sys_belong) values (774, 554, '物料申请', 'modules/operationManage/stoMaterialRequisition.html', null, 1, null, 22, 1, null, null, null, 'o2o');
+insert into o2o.sys_menu (menu_id, parent_id, name, url, perms, type, icon, order_num, pc_showed, app_code, app_order_num, remark, sys_belong) values (772, 554, '任务管理', 'modules/operationManage/heaTaskManage.html', 'operation:task:definitions:get,operation:task:groups:get', 1, null, 32, 1, null, null, null, 'o2o');
+insert into o2o.sys_menu (menu_id, parent_id, name, url, perms, type, icon, order_num, pc_showed, app_code, app_order_num, remark, sys_belong) values (771, 554, '任务组设置', 'modules/operationManage/heaTaskGroupSettings.html', 'operation:task:groups:get', 1, null, 31, 1, null, null, null, 'o2o');
+insert into o2o.sys_menu (menu_id, parent_id, name, url, perms, type, icon, order_num, pc_showed, app_code, app_order_num, remark, sys_belong) values (770, 554, '物料申请管理', 'modules/operationManage/heaMaterialApplicationManage.html', null, 1, null, 21, 1, null, null, null, 'o2o');
 insert into o2o.sys_menu (menu_id, parent_id, name, url, perms, type, icon, order_num, pc_showed, app_code, app_order_num, remark, sys_belong) values (769, 554, '门店通讯录', 'modules/operationManage/heaStoreAddressList.html', null, 1, null, 1, 1, null, null, null, 'o2o');
 insert into o2o.sys_menu (menu_id, parent_id, name, url, perms, type, icon, order_num, pc_showed, app_code, app_order_num, remark, sys_belong) values (768, 554, '员工通讯录', 'modules/operationManage/heaStaffAddressList.html', null, 1, null, 8, 1, null, null, null, 'o2o');
 insert into o2o.sys_menu (menu_id, parent_id, name, url, perms, type, icon, order_num, pc_showed, app_code, app_order_num, remark, sys_belong) values (766, 554, '门店职能配置', 'modules/operationManage/storeResponsibility.html', null, 1, null, 3, 1, null, null, null, 'o2o');
