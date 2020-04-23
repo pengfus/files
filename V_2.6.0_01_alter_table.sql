2f0facd60dc76d1d1235e87aa86e46a1c52ed93c
Bryce <pfsheng@aoyuan.net>
2020-04-23 10:05:54 +0800

    添加字段

--- V_2.6.0_01_alter_table.sql
+++ V_2.6.0_01_alter_table.sql
@@ -10,3 +10,14 @@ alter table o2o.o2o_erp_stock_inorder
 
 alter table o2o.o2o_erp_stock_inorder
     add column reverse_apply_id bigint comment '售后单ID' after arrive_time;
+
+alter table o2o.o2o_erp_stock_inorder
+    add column sign_status smallint comment '是否完成签收，1-是，2-否' after reverse_apply_id;
+
+alter table o2o.o2o_erp_stock_inorder
+    add column app_order_code varchar(255) comment '订单编号' after sign_status;
+
+
+insert into o2o.o2o_erp_config_param (code, name, value, create_time, update_time)
+values ('AFTER_SALE_SEND_RECEIPT_RESULT_URL', '售后中心-发送收货结果接口（确认收货）', 'http://aftersale.aomygod.api/aftersale/reverse/signShip', now(), now());
+
